10a4a9c3d824541b7995da219d0b80a73fd89e44
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-12-29 01:54:50 +0530

    Undo the previous style of project hero section and reduce the gap with github projects section

--- src/components/GitHubProjects.jsx
+++ src/components/GitHubProjects.jsx
@@ -52,7 +52,7 @@ export function GitHubProjects() {
   const projectsToShow = [...featuredProjects, ...otherProjects];
 
   return (
-    <section className="py-20 bg-gradient-to-t from-secondary/5 via-background to-primary/5">
+    <section className="pt-10 pb-20 bg-gradient-to-t from-secondary/5 via-background to-primary/5">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 text-[var(--hero-heading)]">
           GitHub Projects
